84dbe9dc199e72682d67f983b2eefbcef7975e62
Yellres <2929799602@qq.com>
2021-03-23 22:57:13 +0800

    add: 2个动画

--- src/pages/memory/canvasImg.js
+++ src/pages/memory/canvasImg.js
@@ -5,25 +5,43 @@ import './can.css'
 
 const CanvasImg = () => {
 
-  useEffect(() => {
-    let ctx = myCanvas.current.getContext('2d')
-    let img = new Image()
-    img.src = bg
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0)
-      ctx.clip() 
-      // ctx.fillStyle = '#fff'
-      ctx.fillRect(100, 0, 300, 300)
-    }
-  })
+  // useEffect(() => {
+  //   let ctx = myCanvas.current.getContext('2d')
+  //   let img = new Image()
+  //   img.src = bg
+  //   img.onload = () => {
+  //     ctx.drawImage(img, 0, 0)
+  //     ctx.clip() 
+  //     // ctx.fillStyle = '#fff'
+  //     ctx.fillRect(100, 0, 300, 300)
+  //   }
+  // })
 
-  let myCanvas = useRef(null)
+  // let myCanvas = useRef(null)
 
   return (
     <div className="d">
-      <img src={cy} width={'400'} height={'300'} className="d_img"></img>
-      <canvas ref={myCanvas} width={'400'} height={'300'}></canvas>
+         <div className="test">
+      dsadasdfadf
+      </div>
+      ::before   ::after
+      {/* <img src={cy} width={'400'} height={'300'} className="d_img"></img> */}
+      {/* <canvas ref={myCanvas} width={'400'} height={'300'}></canvas> */}
+      <div className="animate_a">
+      </div>
+
+      多个子div
+      <div className="animate_b">
+        <div className="b_1 b_item"></div>
+        <div className="b_2 b_item"></div>
+        <div className="b_3 b_item"></div>
+        <div className="b_4 b_item"></div>
+      </div>
+
+   
     </div>
+
+
   )
 }
 
